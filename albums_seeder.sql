9c156a1db7663a19ec922a793eb68d409f9f38cd
Brian Barton <brian.n.barton@gmail.com>
2020-09-08 12:59:03 -0600

    where exercises

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,6 +1,6 @@
 USE codeup_test_db;
 
---truncate albums;
+truncate albums;
 
 insert into albums (artist, name, release_date, genre, sales)
 values ('Michael Jackson', 'Thriller', 1982, 'Pop, Rock, R&B', 47.3),
--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,4 +1,4 @@
 USE codeup_test_db;
--- Albums released after 1991
--- select * from albums where release_date > 1991;
---delete from albums where release_date > 1991;
+# Albums released after 1991
+select * from albums where release_date > 1991;
+delete from albums where release_date > 1991;

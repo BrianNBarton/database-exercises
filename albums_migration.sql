d17bec2cd7e58b86cabe05987048167b6a89efc4
Brian Barton <brian.n.barton@gmail.com>
2020-09-07 18:49:50 -0600

    sql albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,22 +1,13 @@
-
-
 USE codeup_test_db;
 
+DROP TABLE IF EXISTS albums;
 
-DROP TABLE albums;
 CREATE TABLE albums (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(200),
-        name VARCHAR(150),
-    release_date INT(4),
-        sales FLOAT(9),
-    genre VARCHAR(300),
-    PRIMARY KEY (id)
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist varchar(100) NOT NULL,
+                        name varchar(100) NOT NULL,
+                        release_date INT NOT NULL,
+                        sales DECIMAL(6, 3) NOT NULL,
+                        genre VARCHAR(100),
+                        PRIMARY KEY (id)
 );
---
--- id — auto-incrementing unsigned integer primary key
--- artist — string for storing the recording artist name
--- name — string for storing a record name
--- release_date — integer representing year record was released
--- sales — floating point value for number of records sold (in millions)
--- genre — string for storing the record's genre(s)
---
+++ delete_exercises.sql
@@ -0,0 +1,4 @@
+USE codeup_test_db;
+-- Albums released after 1991
+-- select * from albums where release_date > 1991;
+--delete from albums where release_date > 1991;
